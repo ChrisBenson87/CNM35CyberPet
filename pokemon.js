4cee52b8f183d95a9214dbb7f4636e53ff20c434
Chris Benson <ChrisBenson87@gmail.com>
2022-03-11 16:09:46 +0000

    done

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,13 @@
 const inquirer = require('inquirer');
-const {questions} = require("./questions");
+const { questions } = require("./questions");
 
-class pokemon {
+class pokemon{
     constructor(name){
         this._name = name;
         this._health = 100;
         this._relationship = 5;
+        this._alive = true;
+        this._status = "Your pokemon is feeling fine.";
     }
 
     get name(){
@@ -20,8 +22,20 @@ class pokemon {
         return this._relationship;
     }
 
+    get alive(){
+      return this._alive;
+    }
+
+    get stats(){
+      console.log({
+        name: this._name,
+        health: this._health,
+        relationship: this._relationship,
+      })
+    }
+
     set name(name){
-      this._name = name
+      this._name = name;
     }
 
     set health(health){
@@ -32,8 +46,14 @@ class pokemon {
       this._relationship = relationship
     }
 
-    eat() {
-      // Do stuff plz        
+    set alive(alive){
+      this._alive = alive
+    }
+
+    checkStats(){
+      if (this._health = 0){
+        this._alive = false;
+      }
     }
 }
 
